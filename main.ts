236dd8b56fc0ba6acccec60bc88b96e3f37605f2
sanchezmigben <82328456+sanchezmigben@users.noreply.github.com>
2023-03-29 16:55:38 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,41 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 5000, 1, 255, 225, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Noise,
+        54,
+        54,
+        255,
+        0,
+        500,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.UntilDone)
+    }
+})
